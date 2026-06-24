6bb7d343bf490029895b51c4b38bbaf887151f91
usernameLo <emailLo@example.com>
2026-06-24 21:44:25 +0700

    fix: replace invalid vector text with shape + splash layout

--- app/src/main/java/com/nexa/nusantara/SplashActivity.java
+++ app/src/main/java/com/nexa/nusantara/SplashActivity.java
@@ -9,6 +9,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
